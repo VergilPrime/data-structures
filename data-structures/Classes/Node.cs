427742c58eacb7e27461b9515bff7b59a4e79bae
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2018-01-03 15:32:10 -0800

    Corrected the whiteboard image

--- data-structures/Classes/Node.cs
+++ data-structures/Classes/Node.cs
@@ -6,20 +6,8 @@ namespace datastructures.Classes
 {
     public class Node
     {
-        public int Value { get; set; }
+        public object Value { get; set; }
 
         public Node Next { get; set; }
-
-        public Node(int value)
-        {
-            Value = value;
-        }
-
-        public Node(int value, Node next)
-        {
-            Value = value;
-            Next = next;
-
-        }
     }
 }
