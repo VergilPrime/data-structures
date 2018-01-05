4b95bc0ab32ce2a903973b87267f9a201f135e36
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2018-01-05 14:40:17 -0800

    Doubly bubbly.

--- data-structures/Classes/DoubleLinkList.cs
+++ data-structures/Classes/DoubleLinkList.cs
@@ -8,33 +8,97 @@ namespace datastructures.Classes
     {
         public DoubleNode Head { get; set; }
 
-        public void Remove(int input)
+        public DoubleNode Runner { get; set; }
+
+        public DoubleLinkList()
         {
-            DoubleNode R = Head;
-            while (R.Next != null)
-            {
-                if(R.Value == input)
-                {
-                    R.Prev.Next = null;
-                    R.Next.Prev = null;
-                    R.Prev = null;
-                    R.Next = null;
-                }
-                R = R.Next;
-            }
+            Head = new DoubleNode { Value = 0, Prev = null, Next = null };
+            Runner = Head;
         }
 
-        public void AddBefore(int input,int target)
+        public DoubleLinkList(int value)
         {
-            DoubleNode R = Head;
-            while (R.Next != null)
-            {
-                if (R.Next.Value == input)
-                {
-
-                    R.Next = new DoubleNode { Prev = R, Value = input, Next = R.Next };
-                }
-            }
+            Head = new DoubleNode { Value = value, Prev = null, Next = null };
+            Runner = Head;
         }
+
+        public void AddFirst(int value)
+        {
+            Runner = Head;
+            Head = new DoubleNode { Prev = null, Value = value, Next = Head };
+
+        }
+
+        public void AddLast(int value)
+        {
+            Runner = Head;
+            while(Runner.Next != null)
+            {
+                Runner = Runner.Next;
+            }
+            Runner.Next = new DoubleNode { Prev = Runner, Value = value, Next = null };
+            Runner = Head;
+        }
+
+        public void AddBefore(int value,int target)
+        {
+            Runner = Head;
+            while (Runner.Next != null)
+            {
+                if(Runner.Value == target)
+                {
+                    DoubleNode node = new DoubleNode { Prev = Runner.Prev, Value = value, Next = Runner };
+                    Runner.Prev.Next = node;
+                    Runner.Prev = node;
+                }
+                Runner = Runner.Next;
+            }
+            Runner = Head;
+        }
+
+        public void AddAfter(int value, int target)
+        {
+            Runner = Head;
+            while (Runner.Next != null)
+            {
+                if (Runner.Value == target)
+                {
+                    DoubleNode node = new DoubleNode { Prev = Runner, Value = value, Next = Runner.Next };
+                    Runner.Next.Prev = node;
+                    Runner.Next = node;
+                }
+                Runner = Runner.Next;
+            }
+            Runner = Head;
+        }
+
+        public void RemoveNode(int target)
+        {
+            Runner = Head;
+            Runner = Runner.Next;
+            while (Head.Value == target)
+            {
+                Head = Head.Next;
+                Head.Prev = null;
+                Runner.Next = null;
+                Runner = Head;
+            }
+            while(Runner.Next != null)
+            {
+                if(Runner.Value == target)
+                {
+                    Runner.Next.Prev = Runner.Prev;
+                    Runner.Prev.Next = Runner.Next;
+                    Runner.Next = null;
+                    Runner.Prev = null;
+                }
+                Runner = Head;
+            }
+            Runner = Head;
+        }
+
+
+
+
     }
 }
