5b9913cb1cabd02c397e400bacaf6463263e5b02
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2018-01-08 16:12:53 -0800

    Queue and Stack

---
+++ data-structures/Classes/Queue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace datastructures.Classes
+{
+    public class Queue
+    {
+        public Node Head { get; set; }
+        public Node Runner { get; set; }
+
+        public void NQ(int Val)
+        {
+            Head = new Node { Value = Val, Next = Head };
+        }
+
+        public Node Peek()
+        {
+            Runner = Head;
+            while (Runner.Next!= null)
+            {
+                Runner = Runner.Next;
+            }
+            return Runner;
+        }
+
+        public void DQ()
+        {
+            Runner = Head;
+            while (Runner.Next.Next != null)
+            {
+                Runner = Runner.Next;
+            }
+            Runner.Next = null;
+        }
+    }
+}
